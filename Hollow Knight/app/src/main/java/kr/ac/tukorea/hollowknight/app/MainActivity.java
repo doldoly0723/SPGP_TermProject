ab0d20d7edf75f8757a9efb04db428de264d501f
Kim Mun Su <doldoly0723@naver.com>
2024-05-10 01:23:46 +0900

    title Scene 수정

--- Hollow Knight/app/src/main/java/kr/ac/tukorea/hollowknight/app/MainActivity.java
+++ Hollow Knight/app/src/main/java/kr/ac/tukorea/hollowknight/app/MainActivity.java
@@ -3,6 +3,7 @@ package kr.ac.tukorea.hollowknight.app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,10 +17,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public boolean OnTouchEvent(MotionEvent event){
-        if(event.getAction() == MotionEvent.ACTION_DOWN){
-            startActivity(new Intent(this, HollowKnightActivity.class));
-        }
-        return false;
+//    public boolean OnTouchEvent(MotionEvent event){
+//        if(event.getAction() == MotionEvent.ACTION_DOWN){
+//            startActivity(new Intent(this, HollowKnightActivity.class));
+//        }
+//        return false;
+//    }
+    public void onBtnStartGame(View view){
+        startActivity(new Intent(this, HollowKnightActivity.class));
     }
 }
