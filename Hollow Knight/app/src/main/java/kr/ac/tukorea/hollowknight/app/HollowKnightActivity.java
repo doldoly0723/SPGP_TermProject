a8fcc95a8c2ab7879c87506b7507d60976863a6d
Kim Mun Su <doldoly0723@naver.com>
2024-05-09 23:45:29 +0900

    비어있는 Scene 연결 및 디버그 빌드 수정

--- Hollow Knight/app/src/main/java/kr/ac/tukorea/hollowknight/app/HollowKnightActivity.java
+++ Hollow Knight/app/src/main/java/kr/ac/tukorea/hollowknight/app/HollowKnightActivity.java
@@ -2,18 +2,19 @@ package kr.ac.tukorea.hollowknight.app;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import kr.ac.tukorea.framework.activity.GameActivity;
+import kr.ac.tukorea.framework.scene.Scene;
+import kr.ac.tukorea.hollowknight.BuildConfig;
 
-import kr.ac.tukorea.hollowknight.R;
 
-public class HollowKnightActivity extends AppCompatActivity {
+public class HollowKnightActivity extends GameActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Scene.drawDebugInfo = BuildConfig.DEBUG;
         super.onCreate(savedInstanceState);
+        // Scene.drawsDebugInfo 변경 시점에 주의한다.
+        // new GameView() 가 호출되는 super.onCreate() 보다 이전에 해야 한다.
+        new Scene().push();
     }
 }
--- Hollow Knight/app/src/main/java/kr/ac/tukorea/hollowknight/app/MainActivity.java
+++ Hollow Knight/app/src/main/java/kr/ac/tukorea/hollowknight/app/MainActivity.java
@@ -1,12 +1,10 @@
 package kr.ac.tukorea.hollowknight.app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import kr.ac.tukorea.hollowknight.R;
 
@@ -15,12 +13,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    }
+
+    public boolean OnTouchEvent(MotionEvent event){
+        if(event.getAction() == MotionEvent.ACTION_DOWN){
+            startActivity(new Intent(this, HollowKnightActivity.class));
+        }
+        return false;
     }
 }
