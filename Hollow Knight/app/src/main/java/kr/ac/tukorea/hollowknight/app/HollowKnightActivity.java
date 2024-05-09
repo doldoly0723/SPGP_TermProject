08fdae670bb9693d8efe7b00b455b1d0472cd95d
Kim Mun Su <doldoly0723@naver.com>
2024-05-09 23:47:57 +0900

    landscape만 지원하도록 설정

--- Hollow Knight/app/src/main/java/kr/ac/tukorea/hollowknight/app/HollowKnightActivity.java
+++ Hollow Knight/app/src/main/java/kr/ac/tukorea/hollowknight/app/HollowKnightActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import kr.ac.tukorea.framework.activity.GameActivity;
 import kr.ac.tukorea.framework.scene.Scene;
+import kr.ac.tukorea.framework.view.Metrics;
 import kr.ac.tukorea.hollowknight.BuildConfig;
 
 
@@ -12,6 +13,7 @@ public class HollowKnightActivity extends GameActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Scene.drawDebugInfo = BuildConfig.DEBUG;
+        Metrics.setGameSize(16,9);
         super.onCreate(savedInstanceState);
         // Scene.drawsDebugInfo 변경 시점에 주의한다.
         // new GameView() 가 호출되는 super.onCreate() 보다 이전에 해야 한다.
